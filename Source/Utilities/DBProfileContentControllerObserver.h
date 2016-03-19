1e6472c77423de33306b1e099fa97bf6e2915bb9
Devon Boyer <devonboyer94@gmail.com>
2016-03-18 23:56:36 -0400

    Add missing files

---
+++ Source/Utilities/DBProfileContentControllerObserver.h
@@ -0,0 +1,35 @@
+//
+//  DBProfileContentControllerObserver.h
+//  DBProfileViewController
+//
+//  Created by Devon Boyer on 2016-03-11.
+//  Copyright (c) 2015 Devon Boyer. All rights reserved.
+//
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+
+#import <Foundation/Foundation.h>
+
+#import "DBProfileContentPresenting.h"
+
+@class DBProfileContentControllerObserver;
+
+@protocol DBProfileContentControllerObserverDelegate <NSObject>
+
+- (void)contentControllerObserver:(DBProfileContentControllerObserver *)observer contentControllerScrollViewDidScroll:(UIScrollView *)scrollView;
+
+@end
+
+@interface DBProfileContentControllerObserver : NSObject
+
+- (instancetype)initWithContentController:(DBProfileContentController *)contentController delegate:(id<DBProfileContentControllerObserverDelegate>)delegate;
+
+@property (nonatomic, weak) id<DBProfileContentControllerObserverDelegate> delegate;
+@property (nonatomic, weak, readonly) DBProfileContentController *contentController;
+@property (nonatomic, assign, getter=isObserving, readonly) BOOL observing;
+
+- (void)startObserving;
+- (void)stopObserving;
+
+@end
---
+++ Source/Utilities/DBProfileContentControllerObserver.m
@@ -0,0 +1,75 @@
+//
+//  DBProfileContentControllerObserver.m
+//  DBProfileViewController
+//
+//  Created by Devon Boyer on 2016-03-11.
+//  Copyright (c) 2015 Devon Boyer. All rights reserved.
+//
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+
+#import "DBProfileContentControllerObserver.h"
+
+static void * DBProfileContentControllerObserverKVOContext = &DBProfileContentControllerObserverKVOContext;
+static NSString * const DBProfileContentControllerObserverContentOffsetKeyPath = @"contentOffset";
+
+@interface DBProfileContentControllerObserver ()
+@property (nonatomic, weak) DBProfileContentController *contentController;
+@property (nonatomic, assign, getter=isObserving) BOOL observing;
+@end
+
+@implementation DBProfileContentControllerObserver
+
+- (instancetype)initWithContentController:(DBProfileContentController *)contentController delegate:(id<DBProfileContentControllerObserverDelegate>)delegate {
+    self = [super init];
+    if (self) {
+        self.delegate = delegate;
+        self.contentController = contentController;
+    }
+    return self;
+}
+
+- (void)dealloc {
+    if (self.isObserving) {
+        [self stopObserving];
+    }
+}
+
+- (void)startObserving {
+    if (self.isObserving) return;
+
+    self.observing = YES;
+    
+    UIScrollView *scrollView = [self.contentController contentScrollView];
+    
+    if (scrollView) {
+        [scrollView addObserver:self
+                     forKeyPath:DBProfileContentControllerObserverContentOffsetKeyPath
+                        options:0
+                        context:&DBProfileContentControllerObserverKVOContext];
+    }
+}
+
+- (void)stopObserving {
+    if (!self.isObserving) return;
+    
+    self.observing = NO;
+    
+    UIScrollView *scrollView = [self.contentController contentScrollView];
+    
+    if (scrollView) {
+        [scrollView removeObserver:self
+                        forKeyPath:DBProfileContentControllerObserverContentOffsetKeyPath];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:DBProfileContentControllerObserverContentOffsetKeyPath] && context == DBProfileContentControllerObserverKVOContext) {
+        UIScrollView *scrollView = (UIScrollView *)object;
+        [self.delegate contentControllerObserver:self contentControllerScrollViewDidScroll:scrollView];
+    }
+}
+
+@end
