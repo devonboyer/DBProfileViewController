dc7845b8faff90146dc1433ddd35aef536f505d9
Devon Boyer <devonboyer94@gmail.com>
2016-04-17 08:56:58 -0400

    Add untracked files

---
+++ Source/Views/Private/DBProfileTintView.h
@@ -0,0 +1,15 @@
+//
+//  DBProfileTintView.h
+//  DBProfileViewController
+//
+//  Created by Devon Boyer on 2016-04-08.
+//  Copyright (c) 2015 Devon Boyer. All rights reserved.
+//
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBProfileTintView : UIView
+
+@end
